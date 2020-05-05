5c4ba8b5219b9b9223b2d4a5de36c0f87da66079
Yurgus <yugus@mail.ru>
2020-05-05 22:23:57 +0300

    Update get_chat_members.py

--- userge/plugins/get_chat_members.py
+++ userge/plugins/get_chat_members.py
@@ -1,9 +1,13 @@
 """This example shows how to get all the members of a chat."""
 
 from pyrogram import Client
+from datetime import datetime
+from userge import userge, Message
 
-app = Client("my_account")
-target = "pyrogramchat"  # Target channel/supergroup
+
+@userge.on_cmd("/users", about={'header': "Где ж ты?"})
+async def app = Client("my_account")
+target = "LOG_CHANNEL_ID"  # Target channel/supergroup
 
 with app:
     for member in app.iter_chat_members(target):
